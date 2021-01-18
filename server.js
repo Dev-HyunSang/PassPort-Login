b407c357f1ceb8cf5ee254874fc3275ed59237c6
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-19 00:47:58 +0900

    [UPDATE🛠] Server & passport config JavaScript

---
+++ server.js
@@ -0,0 +1,92 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const session = require('express-session')
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+ 
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport, 
+    email =>  users.find(user => user.email === email),
+    id => users.find(user => user.id === id),
+);
+
+const users = [];
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('__method'));
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name });
+});
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs'); 
+}); 
+
+app.post('/register', async (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    try {
+        const hashedPassword =  await bcrypt.hash(password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: name,
+            email: email,
+            password: password
+        });
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log(users);
+})
+
+app.delete('/logout',(req, res) => {
+    req.logOut();
+    res.redirect('/login');
+});
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    next();
+}
+
+app.listen(3000, () => {
+    console.log('Runing Server on Port 3000 / http://127.0.0.1:3000');
+});
